cde89169517256fb5aa47e6213f0ebf0cd3e6ba5
suriyapi <suriya.p@ku.th>
2025-12-29 16:52:48 +0700

    js file

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+/*
+loop statements:
+while
+for
+*/ 
+let counter = 0;
+while(counter <10){
+    console.log('Hello World');
+    //counter = counter + 1;
+    counter += 1;
+}
+
+for(let i=0; i<10; i++){
+    console.log('Hello World from for loop');
+}
