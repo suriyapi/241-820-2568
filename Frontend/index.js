be37a6826a8fbe0d848688ed5ebb5e8f64276e53
suriyapi <suriya.p@ku.th>
2026-03-09 16:25:19 +0700

    fix error

--- Frontend/index.js
+++ Frontend/index.js
@@ -49,13 +49,13 @@ const submitData = async () => {
         }
         console.log('submitData', userData);
 
-        const errors = validateData(userData);
-        if (errors.length > 0) {
-            throw {
-                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
-                errors: errors
-            }
-        }
+        //   const errors = validateData(userData);
+        //   if (errors.length > 0) {
+        //       throw {
+        //          message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+        //          errors: errors
+        //      }
+        //  }
 
         const response = await axios.post('http://localhost:8000/users', userData);
         console.log('response', response);
@@ -64,9 +64,13 @@ const submitData = async () => {
     } catch (error) {
         console.log('error message', error.message);
         console.log('error', error.errors);
-      //  if (error.response) {
-      //      console.log('Error response:', error.response.data.message);
-      //  }
+        if (error.response) {
+            console.log('error response', error.response);
+            error.message = error.response.data.message;
+            error.errors = error.response.data.errors;
+            
+        }
+
         let htmlData = '<div>'
         htmlData += `<div>${error.message}</div>`;
         htmlData += '<ul>';
